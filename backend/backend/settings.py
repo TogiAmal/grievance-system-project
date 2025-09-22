380526ec07175ed3ddec34aad26c62e88d81613d
Amal Togi <josemontogi2006@gmail.com>
2025-09-22 08:45:12 +0530

    Update ALLOWED_HOSTS for production

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-n7yg&)%m6^3zq^=(v+r(k
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 # ALLOWED_HOSTS will be set automatically by Render
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://grievance-backend-v7ck.onrender.com']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
